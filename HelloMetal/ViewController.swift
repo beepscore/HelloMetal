81ffb755d72f6372a4a2847487889efd7cdbde17
beepscore <support@beepscore.com>
2015-01-11 14:35:13 -0800

    In ViewController add property metalLayer: CAMetalLayer

--- HelloMetal/ViewController.swift
+++ HelloMetal/ViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 import Metal
+import QuartzCore
 
 class ViewController: UIViewController {
 
     // device must be an optional, because it's not set in init.
     // Use ! to implicitly unwrap.
     var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         device = MTLCreateSystemDefaultDevice()
+
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .BGRA8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
     }
 
     override func didReceiveMemoryWarning() {
